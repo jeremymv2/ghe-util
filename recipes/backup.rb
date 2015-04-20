371a1aed57534836221656ff1316d0a74db8aeb7
Jeremy J. Miller <jeremy.miller2@e-hps.com>
2015-04-20 15:11:09 -0400

    changing backup from script to crontab

--- recipes/backup.rb
+++ recipes/backup.rb
@@ -7,12 +7,10 @@
 # All rights reserved - Do Not Redistribute
 #
 
-script "backup" do
-  interpreter "bash"
+cron "ghe-backup" do
+  command "cd /etc/ghe-util/ && ./bin/ghe-backup >> /etc/ghe-util/backup.log 2>&1"
   user "root"
-  cwd node["ghe-util"]["root_dir"]
-  code <<-EOH
-  bin/ghe-backup
-  EOH
+  hour '7'
+  minute '45'
   only_if has_ssh?
 end
